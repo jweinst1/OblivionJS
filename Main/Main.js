a65c2fb15addc971bd1c991b60d77c509ebf4311
Joshua Weinstein <jweinst1@berkeley.edu>
2016-01-03 18:44:40 -0800

    more implementations

--- Main/Main.js
+++ Main/Main.js
@@ -6,24 +6,28 @@
 var Oblivion = (function () {
     function Oblivion() {
         this.arithmetic = new arithopers();
+        this.values = new valueopers();
     }
     //only works on integers and math processing so far.
+    //takes an array of tokens.
     Oblivion.prototype.process = function (commands) {
-        var tokens = commands.split(" ");
-        var currentvar = null;
-        for (var num in tokens) {
-            var token = tokens[num].split("->");
-            currentvar = this.arithmetic[token[0]](token[1]);
+        var current = null;
+        for (var num in commands) {
+            switch (commands[num].code) {
+                case "int":
+                    current = this.values[commands[num].code](commands[num].val);
+                    break;
+                default:
+                    current = this.arithmetic[commands[num].code](parseInt(commands[num].val));
+                    break;
+            }
         }
-        return currentvar;
+        return current;
     };
     return Oblivion;
 })();
 var arithopers = (function () {
     function arithopers() {
-        this.int = function (amount) {
-            return new integer(parseInt(amount));
-        };
         this.add = function (num, amount) {
             num.increment(amount);
             return num;
@@ -46,6 +50,17 @@ var arithopers = (function () {
         };
     }
     return arithopers;
+})();
+var valueopers = (function () {
+    function valueopers() {
+        this.int = function (value) {
+            return new integer(parseInt(value));
+        };
+        this.binary = function (value) {
+            return new binarynum();
+        };
+    }
+    return valueopers;
 })();
 var integer = (function () {
     function integer(value) {
@@ -71,4 +86,53 @@ var integer = (function () {
     };
     return integer;
 })();
+var binarynum = (function () {
+    function binarynum(value) {
+        if (value === void 0) { value = 0; }
+        this.value = value;
+    }
+    binarynum.prototype.tostring = function () {
+        return this.value.toString(2);
+    };
+    binarynum.prototype.increment = function (amount) {
+        this.value += amount;
+    };
+    binarynum.prototype.decrement = function (amount) {
+        this.value -= amount;
+    };
+    binarynum.prototype.multiply = function (amount) {
+        this.value *= amount;
+    };
+    binarynum.prototype.divide = function (amount) {
+        this.value /= amount;
+    };
+    binarynum.prototype.remainder = function (amount) {
+        this.value %= amount;
+    };
+    return binarynum;
+})();
+var Tokenizer = (function () {
+    function Tokenizer() {
+    }
+    Tokenizer.tokenize = function (input) {
+        var tokenstrs = input.split(" ");
+        var tokens = [];
+        for (var num in tokenstrs)
+            tokens.push(new Token(tokenstrs[num]));
+        return tokens;
+    };
+    return Tokenizer;
+})();
+//main Token class
+var Token = (function () {
+    function Token(input) {
+        var pair = input.split("->");
+        this.val = pair[1];
+        this.code = pair[0];
+    }
+    return Token;
+})();
+var test = new Oblivion();
+var putin = Tokenizer.tokenize("int->5 add->4 sub->6 mul->4");
+console.log(test.process(putin));
 //# sourceMappingURL=Main.js.map
--- Main/Main.ts
+++ Main/Main.ts
@@ -6,32 +6,34 @@
 //top level virtual machine object
 class Oblivion {
     arithmetic:arithopers;
+    values:valueopers;
     constructor() {
         this.arithmetic = new arithopers();
+        this.values = new valueopers();
     }
     //only works on integers and math processing so far.
-    process(commands:string) {
-        var tokens = commands.split(" ");
-        var currentvar = null;
-        for(var num in tokens) {
-            var token = tokens[num].split("->");
-            currentvar = this.arithmetic[token[0]](token[1]);
+    //takes an array of tokens.
+    process(commands:Token[]) {
+        var current = null;
+        for(var num in commands) {
+            switch(commands[num].code) {
+                case "int": current = this.values[commands[num].code](commands[num].val);
+                    break;
+                default: current = this.arithmetic[commands[num].code](parseInt(commands[num].val));
+                    break;
+            }
         }
-        return currentvar;
+        return current;
     }
 }
 
 class arithopers {
-    int:Function;
     add:Function;
     sub:Function;
     mul:Function;
     div:Function;
     rem:Function;
     constructor() {
-        this.int = function(amount:string) {
-            return new integer(parseInt(amount));
-        };
         this.add = function(num:numvalue, amount) {
             num.increment(amount);
             return num;
@@ -53,6 +55,20 @@ class arithopers {
             return num;
         }
     }
+}
+
+class valueopers {
+    int:Function;
+    binary:Function;
+    constructor() {
+        this.int = function(value:string) {
+            return new integer(parseInt(value));
+        };
+        this.binary = function(value:string) {
+            return new binarynum();
+        }
+    }
+
 }
 //interfaces for numbers
 interface numvalue {
@@ -89,3 +105,51 @@ class integer implements numvalue {
         return this.value.toString();
     }
 }
+
+class binarynum implements numvalue {
+    value:number;
+    constructor(value:number=0) {
+        this.value = value;
+    }
+    tostring() {
+        return this.value.toString(2);
+    }
+    increment(amount:number) {
+        this.value += amount;
+    }
+    decrement(amount:number) {
+        this.value -= amount;
+    }
+    multiply(amount:number) {
+        this.value *= amount;
+    }
+    divide(amount:number) {
+        this.value /= amount;
+    }
+    remainder(amount:number) {
+        this.value %= amount;
+    }
+}
+
+class Tokenizer {
+    static tokenize(input:string) {
+        var tokenstrs = input.split(" ");
+        var tokens = [];
+        for(var num in tokenstrs) tokens.push(new Token(tokenstrs[num]));
+        return tokens;
+    }
+}
+//main Token class
+class Token {
+    code:string;
+    val:string;
+    constructor(input:string){
+        var pair = input.split("->");
+        this.val = pair[1];
+        this.code = pair[0];
+    }
+}
+
+var test = new Oblivion();
+var putin = Tokenizer.tokenize("int->5 add->4 sub->6 mul->4");
+console.log(test.process(putin));
